a527d3d8436dc283041b125d12cdc7951b0559f0
Nikolay Karadzhov <nkaradzhov89@gmail.com>
2025-01-09 23:46:56 +0200

    cleanup, add error messages

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::fs;
 use std::io::Read;
 use std::io::Write;
 
+use anyhow::anyhow;
+use anyhow::Context;
 use clap::ArgAction;
 use clap::Parser;
 use clap::Subcommand;
@@ -29,8 +31,8 @@ enum Commands {
 
     #[command(arg_required_else_help = true)]
     CatFile {
-        #[arg(short, long, action(ArgAction::SetTrue))]
-        ppp: bool,
+        #[arg(name("p"), short, long, action(ArgAction::SetTrue))]
+        p: bool,
         hash: String,
     },
     #[command(arg_required_else_help = true)]
@@ -41,47 +43,43 @@ enum Commands {
     },
 }
 
-
 fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
 
     match args.command {
-        Commands::Init => {
-            return init();
-        }
-        Commands::CatFile { hash, ppp } => {
-            return cat_file(hash);
-        }
-        Commands::HashObject { write, path } => {
-            return hash_object(write, path);
-        }
+        Commands::Init => init(),
+        Commands::CatFile { hash, p: _ } => cat_file(hash),
+        Commands::HashObject { write, path } => hash_object(write, path),
     }
 }
 
 fn init() -> anyhow::Result<()> {
-    fs::create_dir(".git")?;
-    fs::create_dir(OBJECTS_DIR)?;
-    fs::create_dir(".git/refs")?;
-    fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
-    Ok(println!("Initialized git directory"))
+    fs::create_dir(".git").context("Could not create .git directory")?;
+    fs::create_dir(OBJECTS_DIR).context(format!("Could not create {OBJECTS_DIR} directory"))?;
+    fs::create_dir(".git/refs").context("Could not create .git/refs directory")?;
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").context("Could not write to .git/HEAD")?;
+    println!("Initialized git directory");
+    Ok(())
 }
 
 fn cat_file(hash: String) -> anyhow::Result<()> {
-    //args[2] is the subcommand, -p, blob, tree, commit, etc
-    // cat-file -p -> automatically determine object type
-    // let object_hash = &args[3];
     let (dir, file) = hash.split_at(2);
 
     let full_path = format!("{OBJECTS_DIR}/{dir}/{file}");
 
-    let encoded_contents = fs::read(full_path)?;
+    let encoded_contents =
+        fs::read(&full_path).context(format!("File does not exist {full_path}"))?;
     let mut decoder = ZlibDecoder::new(&*encoded_contents);
     let mut contents = String::new();
-    decoder.read_to_string(&mut contents)?;
+    decoder
+        .read_to_string(&mut contents)
+        .context(format!("Failed to decompress file {full_path}"))?;
 
     let mut split = contents.split("\0");
-    let contents = split.nth(1).expect("expect file to have contents");
-    Ok(print!("{}", contents))
+    // let contents = split.nth(1).expect("expect file to have contents");
+    let contents = split.nth(1).ok_or_else(|| anyhow!("No Contents!"))?;
+    print!("{}", contents);
+    Ok(())
 }
 
 fn hash_object(write: bool, path: String) -> anyhow::Result<()> {
@@ -99,12 +97,19 @@ fn hash_object(write: bool, path: String) -> anyhow::Result<()> {
 
     if write {
         let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
-        encoder.write_all(contents.as_bytes())?;
-        let compressed = encoder.finish()?;
+        encoder
+            .write_all(contents.as_bytes())
+            .context("Could not compress file contents")?;
+        let compressed = encoder
+            .finish()
+            .context("Could not compress file contents")?;
         let (dir, file) = hash.split_at(2);
         let path = format!("{OBJECTS_DIR}/{dir}/{file}");
-        fs::create_dir_all(format!("{OBJECTS_DIR}/{dir}"))?;
-        fs::write(path, compressed)?;
+        let directory = format!("{OBJECTS_DIR}/{dir}");
+        fs::create_dir_all(&directory)
+            .context(format!("Could not create directory {directory}"))?;
+        fs::write(&path, compressed).context(format!("Could not write file {path}"))?;
     }
-    Ok(println!("{}", String::from(hash)))
+    println!("{}", hash);
+    Ok(())
 }
